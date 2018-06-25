dff741b2f43f37bf7dbf08cdd39338e8cbaa90e7
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2018-06-25 18:46:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -38,18 +38,27 @@ if(message.content.startsWith("a_hug")){
             message.channel.send({embed})
         }
         if (hug == 2){
-            message.channel.send("", {
-                file: "https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif"
-            })}
+            const embed = new Discord.RichEmbed()
+            .setTitle(``)
+            .setDescription(`**${message.author}** fais un câlin à ${message.mentions.users.first()}`)
+            .setColor(0x66c2ff)
+            .setImage("https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif")
+            message.channel.send({embed})
+        }  
+          
 if (hug == 3){
-message.channel.send("", {
-   file: "http://i.imgur.com/27gNd4G.gif"
-   })}
+    const embed = new Discord.RichEmbed()
+    .setTitle(``)
+    .setDescription(`**${message.author}** fais un câlin à ${message.mentions.users.first()}`)
+    .setColor(0x66c2ff)
+    .setImage("http://i.imgur.com/27gNd4G.gif")
+    message.channel.send({embed})
+}
 }
 }
 )
 function random(min, max) {
     min = Math.ceil(0)
-    max = Math.floor(1)
+    max = Math.floor(3)
    hug = Math.floor(Math.random() * (max - min +1) + min);
  }
