602d85644c218944014db20c1e456a75b595d369
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-06-26 07:18:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const client = new Discord.Client();
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({ histoires: [],  xp: []}).write()
  
  var prefix = ("a_")
  var randnum = 0;
@@ -23,6 +32,28 @@ member.guild.channels.find("name", "général").send(`Bienvenue ${member} sur le
  
 
  bot.on('message', message => {
+
+    var msgauthor = message.author.id;
+    if (message.author.bot)return;
+    if(!db.get("xp").find({user: msgauthor}).value()){
+    db.get("xp").push({user: msgauthor, xp: 1}).write();   
+    }else{
+        var userxpdb = db.get("xp").filter({user: msgauthor}).find('xp').value();
+        console.log(userxpdb);
+        var userxp = Object.values(userxpdb)
+        console.log(userxp)
+        console.log(`Niveau : ${userxp[1]}`)
+        db.get("xp").find({user: msgauthor}).assign({user: msgauthor, xp: userxp[1]+= 1}).write();
+    }
+        if (message.content === prefix + "level"){
+            var xp = db.get("xp").filter({user: msgauthor}).find("xp").value()
+            var xpfinal = Object.values(xp);
+            var xp_embed = new Discord.RichEmbed()
+            .setColor(0x66c2ff)
+            .addField (`Niveau de ${message.author.username}`, `Vous avez ${xpfinal[1]} d'XP !`)
+            .setThumbnail(message.author.avatarURL)
+            message.channel.send({embed: xp_embed});
+        }
 
 if(message.content.startsWith("a_test")){
     message.channel.send(`**${message.author.username}** fait apparaître un test avec succéés !`)
@@ -70,6 +101,20 @@ if(hug == 4){
     .setImage("https://i.4pcdn.org/s4s/1516080224529.gif")
     message.channel.send({embed})
 }
+}
+if (message.content.startsWith("a_sondage")){
+    let args = message.content.split(" ").slice(1)
+    let thingToEcho = args.join(" ")
+    var embed = new Discord.RichEmbed()
+    .setDescription(`Sondage lancé Par **${message.author.username}**`)
+    .addField(thingToEcho, "Vous pouvez répondre par les réactions ❌ ou ✔")
+    .setColor(0x66c2ff)
+    message.channel.send(embed)
+    .then(function (message) {
+message.react("❌")
+message.react("✔")    
+    }).catch(function() {
+    });
 }
 }
 )
