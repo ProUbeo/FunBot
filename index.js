19caabe16c661144211d3ebfb651427cd67deaa2
hugo59191 <38160383+hugo59191@users.noreply.github.com>
2018-06-27 17:57:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ member.guild.channels.find("name", "général").send(`Bienvenue ${member} sur le
  
 
  bot.on('message', message => {
+    if (message.content === prefix + "ServeurListe"){
+        message.channel.send(bot.guilds.map(r => r.name + ` | **${r.memberCount}** membres`))
+        }
 
     var msgauthor = message.author.id;
     if (message.author.bot)return;
@@ -58,34 +61,6 @@ member.guild.channels.find("name", "général").send(`Bienvenue ${member} sur le
 if(message.content.startsWith(prefix + "test")){
     message.channel.send(`**${message.author.username}** fait apparaître un test avec succéés !`)
 }
-if(message.content.startsWith(prefix + "kill")){
-    random();
-    if (kill == 1){
-            const embed = new Discord.RichEmbed()
-            .setTitle(``)
-            .setDescription(`**${message.author}** Tue ${message.mentions.users.first()}`)
-            .setColor(0x66c2ff)
-            .setImage("http://gifimage.net/wp-content/uploads/2017/09/anime-kill-gif.gif")
-            message.channel.send({embed})
-        }
-        if (kill == 2){
-            const embed = new Discord.RichEmbed()
-            .setTitle(``)
-            .setDescription(`**${message.author}** Tue ${message.mentions.users.first()}`)
-            .setColor(0x66c2ff)
-            .setImage("https://data.whicdn.com/images/277094600/original.gif")
-            message.channel.send({embed})
-        }
-        if (kill == 3){
-            const embed = new Discord.RichEmbed()
-            .setTitle(``)
-            .setDescription(`**${message.author}** Tue ${message.mentions.users.first()}`)
-            .setColor(0x66c2ff)
-            .setImage("https://i.makeagif.com/media/5-04-2015/FQmUz2.gif")
-            message.channel.send({embed})
-        }
-    }
-
 if (message.content.startsWith(prefix + "sondage")){
     let args = message.content.split(" ").slice(1)
     let thingToEcho = args.join(" ")
@@ -100,58 +75,13 @@ message.react("✔")
     }).catch(function() {
     });
 }
-if (message.content === "sigbot!ASDF Movie"){
-    random();
-    if (ASDF == 1){
-        message.channel.send("", {
-            file: "https://media1.tenor.com/images/1e5ea63a7ff58de29efd66799e10bf9a/tenor.gif"
-        })
-}
-if (ASDF == 2){
-    message.channel.send("", {
-        file: "http://garfie.g.a.pic.centerblog.net/b290ac08.gif"
-    })
-}   if (ASDF == 3){
-    message.channel.send("", {
-        file: "http://gifimage.net/wp-content/uploads/2017/09/asdf-movie-gif-7.gif"
-    })
-}   if (ASDF == 4){
-    message.channel.send("", {
-        file: "https://media.giphy.com/media/XKnvIs2ELq7du/giphy.gif"
-    })
-}   if (ASDF == 5){
-    message.channel.send("", {
-        file: "https://media1.tenor.com/images/b5c745a817f0ef09a827bb48d83526be/tenor.gif"
-    })}  
-     if (ASDF == 6){
-    message.channel.send("", {
-        file: "https://vignette.wikia.nocookie.net/asdfmovie/images/7/73/You_Know_Who%27s_Gay%3F.gif/revision/latest?cb=20140307001053"
-    })
-}   if (ASDF == 7){
-    message.channel.send("", {
-        file: "https://i.imgur.com/5cL1Ld6.gif"
-    })
-}   if (ASDF == 8){
-    message.channel.send("", {
-        file: "https://i.pinimg.com/originals/6a/85/dd/6a85dd96d535832c22d5d5d34620615c.gif"
-    })
-}   if (ASDF == 9){
-    message.channel.send("", {
-        file: "https://i.pinimg.com/originals/c8/e2/29/c8e22909763d09c9ff31924ca150ac27.gif"
-    })
-}   if (ASDF == 10){
-    message.channel.send("", {
-        file: "https://i.imgur.com/P7BBAXh.gif"
-    })
-}
-}
 }
 )
 
 function random(min, max) {
     min = Math.ceil(0)
     max = Math.floor(1)
-   ASDF = Math.floor(Math.random() * (max - min +1) + min);
+   hug = Math.floor(Math.random() * (max - min +1) + min);
  }
 
  function random(min, max) {
