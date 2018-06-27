2243c7e7663a480377452c609055f603f50db061
ProUbeo <38327240+ProUbeo@users.noreply.github.com>
2018-06-27 11:45:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -129,13 +129,12 @@ if(message.content.startsWith("a_kill")){
             message.channel.send({embed})
         }
     }
-    if (message.content.startsWith("a_clear")){
-        if (message.member.hasPermission("MANAGE_MESSAGE")){
-        message.channel.fetchMessage()
-        .then(function(list){
-        message.channel.bulkDelete(list);
-        }, function(err){message.channel.send("Une erreur sauvage apparait !")})}}
 
+    if(message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("pouf !");
+    if(args[0]) return message.channel.send("pouf !")
+    message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.send(`${args0} messages ont été supprimés !`).then(msg => msg.delete(5000));
+    })
 if (message.content.startsWith("a_sondage")){
     let args = message.content.split(" ").slice(1)
     let thingToEcho = args.join(" ")
